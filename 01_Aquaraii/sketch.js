a2f3a3d0b812ba20661d1b59dffe4618970ab4d4
Cody Fuller <git@codyfuller.com>
2017-02-15 22:09:36 -0600

    Add starting planets

--- 01_Aquaraii/sketch.js
+++ 01_Aquaraii/sketch.js
@@ -3,6 +3,9 @@ var xOff;
 var yOff;
 var colorOffset;
 
+var rendered = false;
+var saveImg = false;
+
 function setup() {
     createCanvas(640, 640, WEBGL);
     colorMode(HSB);
@@ -16,22 +19,32 @@ function setup() {
 }
 
 function draw() {
+    if (rendered == false){
+        generatePlanet();
+        rendered = true;
+    }
+    if (saveImg == true){
+        saveCanvas();
+        saveImg = false;
+    }
+}
+
+function generatePlanet(){
     background(20);
     renderNoise();
     pg.filter(BLUR, 2);
     texture(pg)
     rotateY(radians(90));
     sphere(250, 60, 60);
-    noLoop();
 }
 
 function renderNoise(){
     pg.loadPixels();
     for (var x = 0; x < pg.width * 2; x++ ){
         for (var y = 0; y < pg.height * 2; y++){
-            var px_h = (noise(x * 0.1, y * 0.05) * 255) + colorOffset;
-            var px_s = (noise(x, y) * 40) + 20;
-            var px_b = (noise(x * 0.02, y * 0.03) * 65) + 5;
+            var px_h = (noise(x * 0.05, y * 0.2) * 255) + colorOffset;
+            var px_s = (noise(x, y) * 40) + random(40) + 20;
+            var px_b = (noise(x * 0.02, y * 0.1) * 60) + 10;
             var c = color(px_h, px_s, px_b);
             pg.set(x, y, c);
         }
